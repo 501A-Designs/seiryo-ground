6b3e30fe94237bd384d010a350275f2e47dbecbd
501A <art71903design@gmail.com>
2022-08-25 21:09:22 +0900

    updated styles

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,9 @@ import End from '../lib/End'
 import Modal from 'react-modal';
 import FetchSinglePlace from '../lib/FetchSinglePlace'
 
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+
+
 const customStyles = {
   content: {
     top: '5%',
@@ -54,8 +57,9 @@ const customStyles = {
   },
   overlay: {
     background: 'linear-gradient(to right,rgba(255,255,255,0) 0%,white 100%)',
-    backdropFilter: `blur(3px)`,
+    // backdropFilter: `blur(3px)`,
     zIndex:20,
+    cursor:'pointer'
   }
 };
 
@@ -103,7 +107,6 @@ export default function Home() {
 
   const [createNew, setCreateNew] = useState(false);
   const createNewRef = useRef();
-  let newPostNumber = 0;
   
   const [menuDisplay, setMenuDisplay] = useState(false);
   useEffect(() => {
@@ -160,9 +163,7 @@ export default function Home() {
         </StaticGrid>
       </Modal>
 
-      <div
-        className={'stickySide'}
-      >
+      <div className={'stickySide'}>
         <section
           style={{
             maxWidth:`${isBrowser ? '300px':'400px'}`,
@@ -177,120 +178,97 @@ export default function Home() {
             zIndex:1
           }}
         >
+          <h2
+            style={{
+              minWidth:'max-content',
+              margin:0,
+              marginTop: '1em',
+              padding:0,
+              writingMode:'vertical-lr',
+              textOrientation:'mixed'
+            }}
+          >
+            SEIRYO GROUND
+            <br/>
+            清涼広場
+          </h2>
           <StaticGrid>
-            <div
-              style={{
-                padding:'1.5em 0'
-              }}
-            >
-              <AlignItems spaceBetween={true} justifyContent={'space-between'}>
-                <h3
-                  className="seiryoGroundFont"
-                  style={{
-                    letterSpacing:'-1.5px',
-                    fontWeight:'normal',
-                    lineHeight:'0.9em',
-                    minWidth:'max-content',
-                    margin:0
+            <StaticGrid>
+              {!user &&
+                <Button
+                  iconPosition={'left'}
+                  icon={<VscSignIn/>}
+                  onClick={()=>signInWithGoogle()}
+                >
+                  Googleでログイン
+                </Button>
+              }
+              <Button
+                iconPosition={'left'}
+                icon={<VscComment/>}
+                onClick={()=>router.push('/news')}
+              >
+                ニュース
+              </Button>
+              <Button
+                iconPosition={'left'}
+                icon={<VscBook/>}
+                onClick={()=>router.push('/about')}
+              >
+                清涼広場について
+              </Button>
+              <Button
+                iconPosition={'left'}
+                icon={<VscGithubAlt/>}
+                onClick={()=>router.push('https://github.com/501A-Designs/seiryo-ground')}
+              >
+                GitHubを開く
+              </Button>
+              {user && 
+                <Button
+                  iconPosition={'left'}
+                  icon={<VscSignOut/>}
+                  onClick={()=>signInWithGoogle()}
+                >
+                  ログアウト
+                </Button>
+              }
+            </StaticGrid>
+            <Footer></Footer>
+          </StaticGrid>
+        </section>
+
+
+        <StaticGrid gap={'0.5em'}>
+          {user &&          
+            <AlignItems spaceBetween margin={'1.5em 0 0 0'}>
+              <AlignItems>
+                <Button
+                  iconPosition={'left'}
+                  icon={<VscAdd/>}
+                  onClick={()=>{
+                    scroll.scrollToTop();
+                    setCreateNew(true);
                   }}
                 >
-                  SEIRYO GROUND
-                </h3>
-                {!isBrowser &&
-                  <Button
-                    iconPosition={'right'}
-                    icon={menuDisplay ? <VscFold/>:<VscMenu/>}
-                    onClick={()=>{menuDisplay ? setMenuDisplay(false):setMenuDisplay(true)}}
-                  />
-                }
+                  場所を追加
+                </Button>
+                <Button
+                  iconPosition={'left'}
+                  icon={<VscHeart/>}
+                  onClick={()=>setModalIsOpen(true)}
+                >
+                  好きな場所
+                </Button>
               </AlignItems>
-            </div>
-
-            {menuDisplay &&
-              <>
-                {isBrowser &&
-                  <>                  
-                    {user ?
-                      <>
-                        <Button
-                          iconPosition={'left'}
-                          icon={<VscAdd/>}
-                          onClick={()=>{
-                            scroll.scrollToTop();
-                            setCreateNew(true);
-                          }}
-                        >
-                          場所を追加
-                        </Button>
-                        <Button
-                          iconPosition={'left'}
-                          icon={<VscHeart/>}
-                          onClick={()=>setModalIsOpen(true)}
-                        >
-                          好きな場所
-                        </Button>
-                      </>:
-                      <Button
-                        iconPosition={'left'}
-                        icon={<VscSignIn/>}
-                        onClick={()=>signInWithGoogle()}
-                      >
-                        GOOGLEでログイン
-                      </Button>
-                    }
-                  </>
-                }
-                <Button
-                  iconPosition={'left'}
-                  icon={<VscComment/>}
-                  onClick={()=>router.push('/news')}
-                >
-                  SG NEWS
-                </Button>
-                <Button
-                  iconPosition={'left'}
-                  icon={<VscBook/>}
-                  onClick={()=>router.push('/about')}
-                >
-                  SGについて
-                </Button>
-                <Button
-                  iconPosition={'left'}
-                  icon={<VscGithubAlt/>}
-                  onClick={()=>router.push('https://github.com/501A-Designs/seiryo-ground')}
-                >
-                  GITHUBを開く
-                </Button>
-                {user && 
-                  <Button
-                    iconPosition={'left'}
-                    icon={<VscSignOut/>}
-                    onClick={()=>signInWithGoogle()}
-                  >
-                    ログアウト
-                  </Button>
-                }
-              </>
-            }
-          </StaticGrid>
-          {isBrowser && 
-            <StaticGrid>
-              {user ? 
-                <div style={{padding:'0em',backgroundColor: 'white'}}>
-                  <AlignItems gap={'1em'}>
-                    <img src={user.photoURL} width="40" height="40" style={{borderRadius: '10px'}}/>
-                    <h3 style={{color:'black',minWidth:'max-content',}}>{user.displayName}</h3>
-                  </AlignItems>
-                </div>:            
-                <p>
-                  外を歩き回り、今までいって良かった場所を一箇所にまとめた場所。カードをクリックして頂くと詳細やレビューを見ることができます。
-                </p>
-              }
-              <Footer></Footer>
-            </StaticGrid>
+              <div style={{padding:'0em',backgroundColor: 'white'}}>
+                <AlignItems gap={'0.5em'}>
+                  <img src={user.photoURL} width="20" height="20" style={{borderRadius: '50px'}}/>
+                  <h4 style={{color:'black',minWidth:'max-content',}}>{user.displayName}</h4>
+                </AlignItems>
+              </div>
+            </AlignItems>
           }
-        </section>
-        <StaticGrid>
           {user ?
             <>
               {createNew && 
@@ -302,46 +280,122 @@ export default function Home() {
               }
             </>:
             <>
+              <div
+                style={{
+                  minHeight: '20vh',
+                  textAlign:'right',
+                  display: 'flex',
+                  justifyContent: 'right'
+                }}
+              >
+                <div
+                  style={{
+                    alignSelf: 'flex-end'
+                  }}
+                >
+                  <p>Photo By <Link href="https://twitter.com/EyesObsolete"><a>@EyesObsolete</a></Link></p>
+                  <h3 style={{ margin:0}}>
+                    清涼広場へようこそ
+                  </h3>
+                  <p style={{ margin:0}}>
+                    DMS Lat: 35° 39' 10.1952''N
+                    DMS Long: 139° 50' 22.1208''E
+                  </p>
+                </div>
+              </div>
               <DistortionCarousel
                 images={[
                   '/sg-mountain.png',
                   '/sg-mountain2.png',
-                  // '/seiryo-green.png',
-                  // '/seiryo-blue.png',
-                  // '/seiryo-red.png',
-                  // '/seiryo-purple.png',
                 ]}
                 displacmentImage={'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/heightMap.png'}
                 speed={5}
               />
-              <div style={{minHeight: '20vh', textAlign:'right'}}>
-                <p>Photo By <Link href="https://twitter.com/EyesObsolete"><a>@EyesObsolete</a></Link></p>
-                <h3>
-                  Welcome to Seiryo Ground<br/>清涼広場へようこそ
-                </h3>
-              </div>
             </>
           }
-          {newPostNumber > 0 && <p>新しい場所追加：{newPostNumber}個</p>}
-          <StaticGrid grid={isBrowser ? '1fr 1fr':'1fr'}>
-            {fetchedData && fetchedData.map(doc => {
-              return (
-                <div
-                  style={{
-                    transformStyle: 'flat',
-                    perspective: '600px'
-                  }}
-                  key={doc.id}
-                >
-                  <PostThumbNail
-                    id={doc.id}
-                    title={doc.data.name}
-                    type={doc.data.type}
-                  />
-                </div>
-              )
-            })}
-          </StaticGrid>
+
+          <h2 style={{marginBottom:'0.1em'}}>Recent</h2>
+          <ResponsiveMasonry
+            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3, 1200:4}}
+          >
+            <Masonry
+              gutter={'0.25em'}
+            >
+              {fetchedData && fetchedData.map(doc => {
+                return (
+                  <div
+                    style={{
+                      transformStyle: 'flat',
+                      perspective: '600px'
+                    }}
+                    key={doc.id}
+                  >
+                    <PostThumbNail
+                      id={doc.id}
+                      title={doc.data.name}
+                      type={doc.data.type}
+                    />
+                  </div>
+                )
+              })}
+            </Masonry>
+          </ResponsiveMasonry>
+
+          <h2 style={{marginBottom:'0.1em'}}>In Tokyo</h2>
+          <ResponsiveMasonry
+            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3, 1200:4}}
+          >
+            <Masonry
+              gutter={'0.25em'}
+            >
+              {fetchedData && fetchedData.map(doc => {
+                return (
+                  <div
+                    style={{
+                      transformStyle: 'flat',
+                      perspective: '600px'
+                    }}
+                    key={doc.id}
+                  >
+                    <PostThumbNail
+                      id={doc.id}
+                      title={doc.data.name}
+                      type={doc.data.type}
+                    />
+                  </div>
+                )
+              })}
+            </Masonry>
+          </ResponsiveMasonry>
+
+          <h2 style={{marginBottom:'0.1em'}}>All Location</h2>
+          <ResponsiveMasonry
+            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3, 1200:4}}
+          >
+            <Masonry
+              gutter={'0.25em'}
+            >
+              {fetchedData && fetchedData.map(doc => {
+                return (
+                  <div
+                    style={{
+                      transformStyle: 'flat',
+                      perspective: '600px'
+                    }}
+                    key={doc.id}
+                  >
+                    <PostThumbNail
+                      id={doc.id}
+                      title={doc.data.name}
+                      type={doc.data.type}
+                    />
+                  </div>
+                )
+              })}
+            </Masonry>
+          </ResponsiveMasonry>
+          {/* <StaticGrid grid={isBrowser ? '1fr 1fr 1fr':'1fr'} gap={'0.25em'}>
+          </StaticGrid> */}
           <End/>
         </StaticGrid>
       </div>
